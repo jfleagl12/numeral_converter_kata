24cec867c2617dc6061dbab7f91ec5f6a9cced8e
Jason Fleagle <jasonfleagle@hotmail.com>
2015-12-22 14:52:12 +0000

    Created two functions for two conditions

--- converter.js
+++ converter.js
@@ -1,16 +1,11 @@
 var num = prompt("Please type in any number or roman numeral");
 
+//Need two different functions: toRoman & toDecimal for conversion of rom numerals to decimals and vice versa
+
 function toRoman(num) {
     
 var decimalValue = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
 var romanNumeral = [ 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I' ];
-
-// Need to create an if/else statement for arabic vs roman numeral.
-// if ();
-//  do something.
-// else ();
-//  do something else.
-
 
 // Next step Loops through the indices of the decimalValue array.
   for (var i = 0; i < decimalValue.length; i++) {
@@ -21,27 +16,29 @@ var romanNumeral = [ 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V',
       num -= decimalValue[i];
     }
   }
-
+    return toRoman(num);
 };
 
 
 
 
-// function toDecimal(num) {
+function toDecimal(num) {
     
-// var decimalValue = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
-// var romanNumeral = [ 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I' ];
+var decimalValue = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
+var romanNumeral = [ 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I' ];
+
+var decimal = '';
+
+  for (var i = 0; i < decimalValue.length; i++) {
+  while (decimalValue[i] <= num) {
+      decimal += romanNumeral[i];
+      num -= decimalValue[i];
+    }
+  }
+  return toDecimal(num);
+ };
 
 
-// };
 
-
-
-// need to execute 2 different functions for user input either decimal or roman numeral.
-//   if num === decimalValue[] {
-//       return toRoman(num);
-// } else num === romanNumeral[] {
-//      return toDecimal(num);
-// };
 
 
