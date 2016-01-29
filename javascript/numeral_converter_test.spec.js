0317274757840d3ba3a80609eff4e7ea1aa9ea77
Jason Fleagle <jasonfleagle@hotmail.com>
2016-01-29 22:07:27 +0000

    Added numeral_converter_test

--- javascript/numeral_converter_test.spec.js
+++ javascript/numeral_converter_test.spec.js
@@ -1,4 +1,4 @@
-var toRoman = require('./roman-numerals');
+var toRoman = require('/converter.js');
 
 describe("toRoman()", function() {
   it("converts 1", function() {
@@ -71,5 +71,82 @@ describe("toRoman()", function() {
 
   it("converts 3000", function() {
     (toRoman(3000)).toEqual('MMM');
+  });
+});
+
+
+var toDecimal = require('/converter.js');
+
+describe("toDecimal()", function() {
+  it("converts I", function() {
+    (toDecimal("I")).toEqual(1);
+  });
+
+  it("converts II", function() {
+    (toDecimal("II")).toEqual(2);
+  });
+
+  it("converts III", function() {
+    (toDecimal("III")).toEqual(3);
+  });
+
+  it("converts IV", function() {
+    (toDecimal("IV")).toEqual(4);
+  });
+
+  it("converts V", function() {
+    (toDecimal('V')).toEqual(5);
+  });
+
+  it("converts VI", function() {
+    (toDecimal('VI')).toEqual(6);
+  });
+
+  it("converts IX", function() {
+    (toDecimal('IX')).toEqual(9);
+  });
+
+  it("converts XXVII", function() {
+    (toDecimal('XXVII')).toEqual(27);
+  });
+
+  it("converts XLVIII", function() {
+    (toDecimal('XLVIII')).toEqual(48);
+  });
+
+  it("converts LIX", function() {
+    (toDecimal('LIX')).toEqual(59);
+  });
+
+  it("converts XCIII", function() {
+    (toDecimal('XCIII')).toEqual(93);
+  });
+
+  it("converts CXLI", function() {
+    (toDecimal('CXLI')).toEqual(141);
+  });
+
+  it("converts CLXIII", function() {
+    (toDecimal('CLXIII')).toEqual(163);
+  });
+
+  it("converts CDII", function() {
+    (toDecimal('CDII')).toEqual(402);
+  });
+
+  it("converts DLXXV", function() {
+    (toDecimal('DLXXV')).toEqual(575);
+  });
+
+  it("converts CMXI", function() {
+    (toDecimal('CMXI')).toEqual(911);
+  });
+
+  it("converts MXXIV", function() {
+    (toDecimal('MXXIV')).toEqual(1024);
+  });
+
+  it("converts MMM", function() {
+    (toDecimal('MMM')).toEqual(3000);
   });
 });
